3d38d77a4586a455700ee736d4485bee63acf14e
Joseph Choi <jdc07@live.com>
2020-04-15 03:22:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,7 @@ class Artist
   end
 
   def save
-    @@all << self     #saves all created instances of Artist class in 
-                      #@@all class variable
+    @@all << self     #saves all created instances of Artist class in                  #@@all class variable
   end
 
   def self.all      #class method all returns all instances of Artist class 
